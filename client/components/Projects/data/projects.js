6962cdb5edd015ca24f35ccbba3eb7785f0979fe
JeanyHuynh1 <jeanyhuynhtn@gmail.com>
2024-01-27 17:42:36 +0500

    Projects: Implemented projects page

---
+++ client/components/Projects/data/projects.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+        title: "Real Estate Showcase Website - Web Design with C#, HTML, XML, CSS3:",
+        description:
+            `Explore my web design skills with the "Real Estate Website," where I seamlessly blend C#, HTML, XML, and CSS3 to create a captivating and user-friendly platform for a fictitious real estate agent.`,
+        logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+        linkText: "View Project",
+        link: "https://github.com",
+    },
+    {
+        title: "Database Management System - SQL Implementation:",
+        description:
+            `Discover my proficiency in database development with the "Database System - SQL Implementation," showcasing QBE grid and SQL queries, along with a focus on normalization and robust security measures.`,
+        logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+        linkText: "View Project",
+        link: "https://github.com",
+    },
+
+    {
+        title: "Recipe Generator Mobile App - Software Requirements Specification:",
+        description:
+            `Immerse yourself in the planning of the "Recipe App" through the Software Requirements Specification. This collaborative document outlines the blueprint for a mobile app generating recipes for home cooks based on available ingredients, demonstrating my strategic planning skills.`,
+        logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+        linkText: "View Project",
+        link: "https://github.com",
+    },
+];
+
+export default projects;
--- client/components/Projects/index.jsx
+++ client/components/Projects/index.jsx
@@ -1,3 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import Project from './components/Project'
+import projects from './data/projects'
+import './projects.styles.css'
+
 export default function Projects() {
-    return  <p>Projects!</p>
+    return (
+        <div className="projects-container">
+            <div className="title">
+                Things I’ve made trying to put my dent in the universe.
+             </div>
+            <div className="intro-container">
+                <div className="description">
+                Welcome to my Projects page, a curated showcase of my diverse skills and accomplishments. Explore the dynamic intersection of creativity and technology with the "Real Estate Showcase Website," a user-oriented platform crafted using C#, HTML, XML, and CSS3. Dive into the world of database development through the "Database Management System - SQL Implementation," where I demonstrate my proficiency in QBE grid, SQL queries, normalization, and robust database security. For a glimpse into my strategic planning abilities, explore the "Recipe Generator Mobile App - Software Requirements Specification," a collaborative endeavor outlining the blueprint for a unique mobile app generating recipes for home cooks based on on-hand ingredients. Each project represents a chapter in my journey, highlighting technical finesse, innovation, and a commitment to excellence. Join me on this virtual tour, and let the projects speak to the depth of my skills and passion for transformative solutions.
+                </div>
+            </div>
+            <div className="all-projects-container">
+			{projects.map((project, index) => (
+				<div className="all-projects-project" key={index}>
+					<Project
+						logo={project.logo}
+						title={project.title}
+						description={project.description}
+						linkText={project.linkText}
+						link={project.link}
+					/>
+				</div>
+			))}
+		</div>
+        </div>
+    )
 }
