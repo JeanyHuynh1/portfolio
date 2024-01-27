db3e27cc22639fb165d673535bf90457b724e62f
JeanyHuynh1 <jeanyhuynhtn@gmail.com>
2024-01-27 19:13:30 +0500

    Contact: Implemented contact page

--- client/components/AboutMe/index.jsx
+++ client/components/AboutMe/index.jsx
@@ -9,11 +9,23 @@ export default function AboutMe() {
                 My name is Thanh Nhi Huynh
              </div>
             <div className="intro-container">
-                <div className="description">
-                As a highly motivated and flexible professional, my story unfolds through experiences that have honed my ability to gather data, analyze information, and prepare comprehensive reports. Beyond the technical realm, discover a communicator adept at fostering strong relationships with diverse stakeholders. Fluent in English and Vietnamese, with conversational abilities in Cantonese, Mandarin, and Japanese, I am equipped to navigate global landscapes. This page delves into the intricacies of my understanding of Agile/Scrum development, offering insights into my collaborative approach and how I navigate the challenges of the software development life cycle. From programming proficiency to tool mastery, this is a narrative of a technologically adept individual poised to make a meaningful impact in the ever-evolving world of software engineering.
+                <div className="description-container">
+                    <div className="description">
+                    As a highly motivated and flexible professional, my story unfolds through experiences that have honed my ability to gather data, analyze information, and prepare comprehensive reports. Beyond the technical realm, discover a communicator adept at fostering strong relationships with diverse stakeholders. Fluent in English and Vietnamese, with conversational abilities in Cantonese, Mandarin, and Japanese, I am equipped to navigate global landscapes. This page delves into the intricacies of my understanding of Agile/Scrum development, offering insights into my collaborative approach and how I navigate the challenges of the software development life cycle. From programming proficiency to tool mastery, this is a narrative of a technologically adept individual poised to make a meaningful impact in the ever-evolving world of software engineering.
+                    </div>
+                <button onClick={handleResumeClick}>
+                        See my resume
+                </button>
                 </div>
                 <img className="image" src="../../assets/profile.jpeg" alt="profile image" />
             </div>
         </div>
     )
+
+    function handleResumeClick() {
+        var resumeUrl = '../../assets/resume.pdf';
+
+        // Open the resume in a new tab
+        window.open(resumeUrl, '_blank');
+    }
 }
--- client/components/ContactMe/index.jsx
+++ client/components/ContactMe/index.jsx
@@ -1,3 +1,33 @@
-export default function ContactMe() {
-    return  <p>ContactMe!</p>
+/* eslint-disable react/no-unescaped-entities */
+import { Link } from 'react-router-dom'
+import './contact.styles.css'
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+            <div className="title">
+                Let's Get Connected
+             </div>
+            <div className="intro-container">
+                <div className="description">
+                Thank you for reaching out. I appreciate your interest and am open to receiving your feedback, questions, or suggestions. For specific inquiries, please email me directly at <a href={`mailto:jeanyhuynhtn@gmail.com`}>jeanyhuynhtn@gmail.com</a> . I strive to respond to all messages within 24 hours, though it may take a bit longer during busy periods. Alternatively, you can use the contact form on my website by filling out the required fields, and I will get back to you promptly. Once again, thank you for your interest, and I look forward to hearing from you!
+                </div>
+            </div>
+            <form action="" className="contact-form">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required className="input-field" />
+
+            <label htmlFor="contactNumber">Contact Number:</label>
+            <input type="tel" id="contactNumber" name="contactNumber" required className="input-field" />
+
+            <label htmlFor="email">Email Address:</label>
+            <input type="email" id="email" name="email" required className="input-field" />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required className="textarea-field"></textarea>
+
+            <Link to='/'><button type="submit" className="submit-button">Submit</button></Link>
+            </form>
+        </div>
+    )
 }
