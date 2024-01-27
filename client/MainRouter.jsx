0152b9e6db5bc3bf5515cc721d2e59ef0c2e466f
JeanyHuynh1 <jeanyhuynhtn@gmail.com>
2024-01-27 13:46:49 +0500

    Implemented different routes for the pages

--- client/MainRouter.jsx
+++ client/MainRouter.jsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import ContactMe from './components/ContactMe'
+import Projects from './components/Projects'
+import Services from './components/Services'
 
 const MainRouter = () => {
     return (
         <div className="">
             <Routes>
-                <Route exact path = "/" element={<Home />} />
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/about" element={<AboutMe />} />
+                <Route exact path = "/contact" element={<ContactMe />} />
+                <Route exact path = "/projects" element={<Projects />} />
+                <Route exact path = "/services" element={<Services />} />
             </Routes>
         </div>
     )
